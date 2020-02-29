1061b241583ba0692c186e9ffbfc5bc03da6ce0c
YuanyangLiao <1262607947@qq.com>
2020-02-29 16:43:02 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-button type="info" @click="loginout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    loginout() {
+      window.sessionStorage.clear(); // 清空token
+      this.$router.push("/login"); // 跳转到登录页
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Message
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message // 将 Message 挂载为 Vue 原型上的一个属性
